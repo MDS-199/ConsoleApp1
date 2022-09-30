2a85c4e6998a609686c6743556336387aca66e32
MDS-199 <114391729+MDS-199@users.noreply.github.com>
2022-09-30 16:17:06 +0500

    #1
    что то добавил

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,11 +1,81 @@
 ﻿using System;
+using System.Reflection.Metadata;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
+        bool IsWorking = true;
+        while(IsWorking)
+        {
+            Console.WriteLine("Введите комманду:");
+
+            string Comm = Console.ReadLine();
+            /*
+
+
+            if (Comm.ToLower().Contains("сумма"))
+            {
+                Console.WriteLine("Введите первую переменную");
+                int A = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите вторую переменную");
+                int B = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Результат: " + (A + B));
+            }
+            else if (Comm.ToLower().Contains("разниц"))
+            {
+                Console.WriteLine("Введите первую переменную");
+                int A = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите вторую переменную");
+                int B = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Результат: " + (A - B));
+            }
+            else
+                Console.WriteLine("Команда не опознана");
+            */
+
+            try
+            {
+                switch (Comm.ToLower())
+                {
+                    case "сумма":
+                        Console.WriteLine("Введите первую переменную");
+                        int A = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Введите вторую переменную");
+                        int B = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Результат: " + (A + B));
+                        break;
+                    case "разница":
+
+                        Console.WriteLine("Введите первую переменную");
+                        int A_ = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Введите вторую переменную");
+                        int B_ = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Результат: " + (A_ - B_));
+                        break;
+                    case "стоп":
+                        IsWorking = false;
+                        break;
+                    default:
+                        Console.WriteLine("Команда не опознана");
+                        break;
+
+                }
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine($"Ошибка не правильный формат данных: {ex.Message}") ;
+                
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Не опознан: {ex.Message}");
+            }
+
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine(Environment.NewLine);
+        }
+       
 
     }
 }
